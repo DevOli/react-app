b61968b7e80c3f47aba757543e4037b420edaf8e
Fernando.Guerrero <Fernando.Guerrero@jala-foundation.org>
2022-02-23 13:32:49 -0600

    Add Icon component

---
+++ src/components/atoms/Icon.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Image, View } from 'react-native';
+import React, { FC } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+
+interface Props {
+    icon: keyof typeof Ionicons.glyphMap;
+}
+
+const Icon: FC<Props> = (props) => {
+  return (
+    <View>
+      <Ionicons
+        name={props.icon}
+        size={24}
+        color="#555" />
+    </View>
+  )
+}
+
+export default Icon;
