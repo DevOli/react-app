517bddefb1d87521cea642149339ddad55d7f702
jplozadach <pablo.lozada@jala-foundation.org>
2022-02-24 19:30:48 -0400

    Buttons actions added

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -1,10 +1,12 @@
-import { View, Image, StyleSheet, Text, Alert, Platform } from 'react-native'
-import React from 'react'
+import { View, Image, StyleSheet, Text, Alert, Platform, ToastAndroid } from 'react-native'
+import React, {useState} from 'react'
 import Label from '../../atoms/labels/Label'
 import ImageAtom from '../../atoms/images/ImageAtom'
 import {Button} from '../../molecules/Button'
 
 export default function Card(props: any) {
+
+  const [description, setDescription] = useState("How to build cross-platform applications using react native.")
   return (
     <View style={styles.container}>
       <View style={styles.image}>
@@ -12,15 +14,16 @@ export default function Card(props: any) {
       </View>
       <View style={styles.cardBody}>
         <View style={styles.title}>
-          <Label textStyle={{fontSize: 20, color: "black", fontWeight: "bold", margin: 0}} text="Hello world"/>
-          <Label textStyle={{fontSize: 16, color: "black", margin: 0}} text="Secondary text"/>
+          <Label textStyle={{fontSize: 20, color: "black", fontWeight: "bold", margin: 0}} text="React native"/>
+          <Label textStyle={{fontSize: 16, color: "black", margin: 0}} text="Course"/>
         </View>
         <View style={styles.description}>
-          <Label textStyle={{fontSize: 16, color: "black", margin: 0}} text="Greyhound divisively hello coldly wonderfully marginally far upon excluding."/>
+          <Label textStyle={{fontSize: 16, color: "black", margin: 0}} text={description}/>
         </View>
         <View style={styles.buttons}>
-          <Button text='ACTION 1' width={120} height={40} />
-          <Button text='ACTION 2' width={120} height={40} />
+          {/* <Button text='More info' width={140} height={40} onPress={() => {ToastAndroid.show('#GoToNextPage', ToastAndroid.SHORT)}}/> */}
+          <Button text='More info' width={140} height={40} onPress={() => {setDescription("React Native brings React's declarative UI framework to iOS and Android. With React Native, you use native UI controls and have full access to the native platform.")}}/>
+          <Button text='Add' width={80} height={40} onPress={() => {ToastAndroid.show('Course added!', ToastAndroid.SHORT)}}/>
         </View>
       </View>
     </View>
@@ -29,6 +32,7 @@ export default function Card(props: any) {
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
     borderRadius: 8,
   },
   ...Platform.select({
@@ -46,6 +50,7 @@ const styles = StyleSheet.create({
     },
     android: {
       container: {
+        margin: 5,
         elevation: 2,
         shadowRadius: 20,
         shadowColor: 'black',
@@ -57,6 +62,7 @@ const styles = StyleSheet.create({
     height: 220,
   },
   cardBody: {
+    width: "100%",
     backgroundColor: "#fff",
     borderColor: "#A9A9A9",
     borderBottomWidth: 1,
@@ -76,6 +82,7 @@ const styles = StyleSheet.create({
   buttons: {
     borderBottomLeftRadius: 8,
     borderBottomRightRadius: 8,
+    justifyContent: "space-between",
     flexDirection: "row",
     paddingVertical: 6,
     paddingHorizontal: 10,
